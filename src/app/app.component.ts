f7c892668f08558a7b5f137cbd94ec058890b447
dezhidki <dezhidki@jyu.fi>
2020-07-08 13:30:38 +0300

    Increase overflow size in tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ import {TableModelProvider} from './data-view.component';
         <div style="margin: auto">
             <app-data-view *ngIf="show" [modelProvider]="dataProvider" [virtualScrolling]="{
             'enabled': true,
-            'viewOverflow': 1,
+            'viewOverflow': 2,
             'borderSpacing': 2}">
                 <thead appFixedData>
                 <th *ngFor="let header of dataCols">{{header}}</th>
